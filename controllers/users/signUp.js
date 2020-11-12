72e73c4f098f73bd0e2e6d6948bf12ec0576f295
yaag19 <m.sun.1290@gmail.com>
2020-11-12 23:51:25 +0900

    feat: create signup

--- controllers/users/signUp.js
+++ controllers/users/signUp.js
@@ -1,5 +1,24 @@
+const { user } = require('../../models');
+
 module.exports = {
-  post: (req, res) => {
-    res.status(200).send('Welcome!! signUp!');
+  post: async (req, res) => {
+    const { nickName, password, email } = req.body;
+    try {
+      const [result, created] = await user.findOrCreate({
+        where: { email },
+        defaults: { password, nickName },
+      });
+
+      if (!created) {
+        res.status(409).send('Already exists email');
+      } else {
+        const data = await result.get({ plain: true });
+        console.log(data);
+        res.status(201).send(data);
+      }
+      res.status(200).send('Welcome Netflex!');
+    } catch (err) {
+      res.status(500).send(err);
+    }
   },
 };
