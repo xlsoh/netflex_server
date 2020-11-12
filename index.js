3d9112b1ce8957cfd0b61e2099328f44ea5e66bf
yaag19 <m.sun.1290@gmail.com>
2020-11-12 11:52:18 +0900

    modefied index.js

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('🌈️ GET request to the Netflex ! ');
+});
+
+app.listen(PORT, () => {
+  console.log(`📢️ Server Listening On ${PORT}`);
+});
